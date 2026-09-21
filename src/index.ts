181b702c384385d51ef9cc0466b3632a25d303c3
Jörg Jooss <joergjo@microsoft.com>
2026-09-21 10:19:59 +0200

    Add sample hook implementation

--- src/index.ts
+++ src/index.ts
@@ -5,19 +5,57 @@ import { logger } from './logger';
 
 let redisClient: RedisClientType;
 
-// app.hook.preInvocation((context: PreInvocationContext) => {
-//     if (context.invocationContext.options.trigger.type === 'eventHubTrigger') {
-//         context.invocationContext.log(
-//             `preInvocation hook executed for event hub function ${context.invocationContext.functionName}`,
+// This a sample setup for OpenTelemetry context propagation in Azure Functions.
+// Unfortunately, we cannot override the hooks registered by the Azure Functions instrumentation
+// package, so our choice is to either work within the constraints of the existing hooks
+// or implement our own context propagation mechanism and don't use the Azure Functions
+// instrumentation at all.
+// const ACTIVE_SPAN_KEY = '__otel_Azure_funtion_active_span';
+// interface Carrier {
+//     traceparent?: string;
+//     tracestate?: string;
+// }
+
+// app.hook.preInvocation(async (context: PreInvocationContext) => {
+//     if (context.invocationContext.options.trigger.type === 'httpTrigger') {
+//         const carrier: Carrier = {
+//             traceparent: context.invocationContext.traceContext.traceParent,
+//         };
+
+//         const currentContext = propagation.extract(ROOT_CONTEXT, carrier);
+
+//         const resource = detectResources();
+//         const span = tracer.startSpan(
+//             context.invocationContext.functionName,
+//             {
+//                 kind: SpanKind.SERVER,
+//                 attributes: resource.attributes,
+//             },
+//             currentContext
 //         );
+//         otelContext.bind(
+//             trace.setSpan(currentContext, span),
+//             context.functionHandler
+//         );
+//         context.hookData[ACTIVE_SPAN_KEY] = span;
 //     }
 // });
 
 // app.hook.postInvocation(async (context: PostInvocationContext) => {
-//     if (context.invocationContext.options.trigger.type === 'eventHubTrigger') {
-//         context.invocationContext.log(
-//             `postInvocation hook executed for event hub function ${context.invocationContext.functionName}`,
-//         );
+//     const span = context.hookData[ACTIVE_SPAN_KEY] as Span;
+//     if (span) {
+//         if (context.invocationContext.options.trigger.type === 'httpTrigger') {
+//             span.setAttribute(
+//                 ATTR_HTTP_RESPONSE_STATUS_CODE,
+//                 (context.result as string) || 0
+//             );
+
+//             if (context.error) {
+//                 span.recordException(context.error as Error);
+//                 span.setStatus({ code: SpanStatusCode.ERROR });
+//             }
+//         }
+//         span.end();
 //     }
 // });
 
